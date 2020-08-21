ef0cfe12495da63417ab18a4c7a4912978cd291a
jwildfire <jwildfire@gmail.com>
2020-08-21 09:24:48 -0400

    delete stray files

--- BuildMeNan.R
+++
@@ -1,6 +0,0 @@
-
-
-library( BaSS )  # Add a few new functions that are useful for package development
-
-AddFunctionToPkg( "BaSSFuncTry", strFunctionDescription = "This function is used to test the AddFunctionToPkg. ")
-
