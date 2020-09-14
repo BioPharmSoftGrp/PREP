842e73b903cfaf5e1f63410103e256b24108bcfa
jwildfire <jwildfire@gmail.com>
2020-09-14 09:26:30 -0400

    Update AddTab.R

--- R/AddTab.R
+++ R/AddTab.R
@@ -6,10 +6,36 @@
 #'
 #' @param strModuleID ID for new module
 #' @param bType Type of application. Valid options are 'standalone' or 'package'.
+#' @param bSubModuleIDs IDs for the sub tabs
 #'
 #' @export
 
 
-AddTab<-function(strModuleID, strType="package"){
-    AddModules(vModuleIDs=c(strModuleID), strType=strType)
+AddTab<-function(strModuleID, strType="package", vSubModuleIDs=c()){
+    # Create module that then calls the submodules
+    subModuleUI<-readLines("path/to/template.r")
+    subModuleServer<-readLines("path/to/template.r")
+    
+    #parameters to be generated from vSubModuleIDs
+    lCustomParameters(
+        SUB_TAB_SERVER = "", 
+        SUB_TAB_UI = "", 
+        SUB_TAB_MENU = "" #update AddModules to be more flexible regarding custom menu items - this is probably the hardest part ... 
+    )
+    
+    AddModules(
+        vModuleIDs=c(strModuleID), 
+        strType=strType
+        strNewModuleUITemplate=subModuleUI
+        strNewModuleServerTemplate=subModuleServer,
+        bDashboard=TRUE, #NOTE: Might not always be the default, but will be always required for this helper function. 
+        lCustomParameters=lCustomParameters
+    )
+
+    #Create subtabs from the standard template (or we can make an option that supports custom templates
+    for(strSubModuleID in vSubModuleIDs){
+        createModule(strModuleID=paste0(strModuleID,"_",strSubModuleID))
+    }
+    
+#NOTE:Another option would be to update AddModules() to allow it to target existing modules, and then just call `AddModules(vModuleIDs = vSubModuleIDs, target=strModuleID, ... )`. This would be nice, but I suspect it would be fairly complex. 
 }
