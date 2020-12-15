15097ee17a2944879da99d87b922e9ccc27f4cdc
jwildfire <jwildfire@gmail.com>
2020-12-15 09:53:56 -0500

    comment out old tests

--- tests/testthat/test-CreateBaSSApp.R
+++ tests/testthat/test-CreateBaSSApp.R
@@ -1,120 +1,117 @@
-context( "CreatePREPApp")
-source( "MiscTestFunctions.R")
+# context( "CreatePREPApp")
+# source( "MiscTestFunctions.R")
 
 test_that("Test- CreatePREPApp", {
 
-    bSuccess <- FALSE
-    strErr   <- "None of the projects were created."
+    # bSuccess <- FALSE
+    # strErr   <- "None of the projects were created."
+ 
+    # strWD               <- getwd()
+    # strProjDir          <- paste( strWD, "TestProject", sep = "/")
+ 
+    # CleanUpAfterTest( strProjDir ) # Clean up just in case a previous test failed and could not clean up
+    # strProjName         <- "TestProj"
+    # strCalcName         <- "TestCalc"
+    # strShinyName        <- "TestShiny"
+    # strShinyDispName    <- "Test App"
+ 
+    # strRet              <- CreatePREPApp( strProjectDirectory       = strProjDir,
+                                            # strProjectName            = strProjName,
+                                            # strCalculationLibraryName = strCalcName,
+                                            # strShinyAppName           = strShinyName,
+                                            # strShinyAppDisplayName    = strShinyDispName,
+# 
+                                            # bCreateWithExampleTabs     = TRUE,
+                                            # bCreateShinyApp            = TRUE,
+                                            # bCreateCalculationPackage  = TRUE,
+                                            # bCreateShinyAppAsPackage   = FALSE
+                                            # )
+ 
+    #Now check that the created project was correctly done.
+    # strProjectDirExp       <- paste( strProjDir, "/", strProjName, sep="" )
+    # bProjectDirectoryCreated <- dir.exists( strProjectDirExp )
+    # expect_true( bProjectDirectoryCreated, info = paste( "The project directory, ", strProjectDirExp, " was not created as expected. ", "Project Directory" ))
+ 
+    # vFilesInProject <- list.files( strProjectDirExp )
+    # nQtyFiles       <- length( vFilesInProject )
+    # nQtyFilesExp    <- 4
+ 
+    # expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
+ 
+    # Now check the Calc package
+    # strTmpDir       <- paste( strProjectDirExp, "/", strCalcName, sep = "" )
+    # nQtyFilesExp    <- 8
+    # nQtyFiles       <- length( list.files( strTmpDir ) )
+    # expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the R library, project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
+ 
+    # Now check the Shiny package
+    # strTmpDir       <- paste( strProjectDirExp, "/", strShinyName, sep = "" )
+    # nQtyFilesExp    <- 13
+    # nQtyFiles       <- length( list.files( strTmpDir ) )
+    # expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the Shiny project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
 
-    strWD               <- getwd()
-    strProjDir          <- paste( strWD, "TestProject", sep = "/")
-
-    CleanUpAfterTest( strProjDir ) # Clean up just in case a previous test failed and could not clean up
-    strProjName         <- "TestProj"
-    strCalcName         <- "TestCalc"
-    strShinyName        <- "TestShiny"
-    strShinyDispName    <- "Test App"
-
-    strRet              <- CreatePREPApp( strProjectDirectory       = strProjDir,
-                                            strProjectName            = strProjName,
-                                            strCalculationLibraryName = strCalcName,
-                                            strShinyAppName           = strShinyName,
-                                            strShinyAppDisplayName    = strShinyDispName,
-
-                                            bCreateWithExampleTabs     = TRUE,
-                                            bCreateShinyApp            = TRUE,
-                                            bCreateCalculationPackage  = TRUE,
-                                            bCreateShinyAppAsPackage   = FALSE
-                                            )
-
+    # CleanUpAfterTest( strProjDir )
+ 
+# })
+# 
+# test_that("Test- CreatePREPAppShinyAsPkg", {
+# 
+    # bSuccess <- FALSE
+    # strErr   <- "None of the projects were created."
+# 
+    # strWD               <- getwd()
+    # strProjDir          <- paste( strWD, "TestProject", sep = "/")
+# 
+    # CleanUpAfterTest( strProjDir ) # Clean up just in case a previous test failed and could not clean up
+    # strProjName         <- "TestProj"
+    # strCalcName         <- "TestCalc"
+    # strShinyName        <- "TestShiny"
+    # strShinyDispName    <- "Test App"
+# 
+    # strRet              <- CreatePREPApp( strProjectDirectory       = strProjDir,
+                                        #   strProjectName            = strProjName,
+                                        #   strCalculationLibraryName = strCalcName,
+                                        #   strShinyAppName           = strShinyName,
+                                        #   strShinyAppDisplayName    = strShinyDispName,
+# 
+                                        #   bCreateWithExampleTabs     = TRUE,
+                                        #   bCreateShinyApp            = TRUE,
+                                        #   bCreateCalculationPackage  = TRUE,
+                                        #   bCreateShinyAppAsPackage   = TRUE
+    # )
+ 
 
     # Now check that the created project was correctly done.
-    strProjectDirExp       <- paste( strProjDir, "/", strProjName, sep="" )
-    bProjectDirectoryCreated <- dir.exists( strProjectDirExp )
-    expect_true( bProjectDirectoryCreated, info = paste( "The project directory, ", strProjectDirExp, " was not created as expected. ", "Project Directory" ))
-
-    vFilesInProject <- list.files( strProjectDirExp )
-    nQtyFiles       <- length( vFilesInProject )
-    nQtyFilesExp    <- 4
-
-    expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
-
-    # Now check the Calc package
-    strTmpDir       <- paste( strProjectDirExp, "/", strCalcName, sep = "" )
-    nQtyFilesExp    <- 8
-    nQtyFiles       <- length( list.files( strTmpDir ) )
-    expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the R library, project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
-
-
-    # Now check the Shiny package
-    strTmpDir       <- paste( strProjectDirExp, "/", strShinyName, sep = "" )
-    nQtyFilesExp    <- 13
-    nQtyFiles       <- length( list.files( strTmpDir ) )
-    expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the Shiny project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
-
-    CleanUpAfterTest( strProjDir )
-
-
-})
-
-test_that("Test- CreatePREPAppShinyAsPkg", {
-
-    bSuccess <- FALSE
-    strErr   <- "None of the projects were created."
-
-    strWD               <- getwd()
-    strProjDir          <- paste( strWD, "TestProject", sep = "/")
-
-    CleanUpAfterTest( strProjDir ) # Clean up just in case a previous test failed and could not clean up
-    strProjName         <- "TestProj"
-    strCalcName         <- "TestCalc"
-    strShinyName        <- "TestShiny"
-    strShinyDispName    <- "Test App"
-
-    strRet              <- CreatePREPApp( strProjectDirectory       = strProjDir,
-                                          strProjectName            = strProjName,
-                                          strCalculationLibraryName = strCalcName,
-                                          strShinyAppName           = strShinyName,
-                                          strShinyAppDisplayName    = strShinyDispName,
-
-                                          bCreateWithExampleTabs     = TRUE,
-                                          bCreateShinyApp            = TRUE,
-                                          bCreateCalculationPackage  = TRUE,
-                                          bCreateShinyAppAsPackage   = TRUE
-    )
-
-
-    # Now check that the created project was correctly done.
-    strProjectDirExp       <- paste( strProjDir, "/", strProjName, sep="" )
-    bProjectDirectoryCreated <- dir.exists( strProjectDirExp )
-    expect_true( bProjectDirectoryCreated, info = paste( "The project directory, ", strProjectDirExp, " was not created as expected. ", "Project Directory" ))
-
-    vFilesInProject <- list.files( strProjectDirExp )
-    nQtyFiles       <- length( vFilesInProject )
-    nQtyFilesExp    <- 4
-
-    expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
+    # strProjectDirExp       <- paste( strProjDir, "/", strProjName, sep="" )
+    # bProjectDirectoryCreated <- dir.exists( strProjectDirExp )
+    # expect_true( bProjectDirectoryCreated, info = paste( "The project directory, ", strProjectDirExp, " was not created as expected. ", "Project Directory" ))
+# 
+    # vFilesInProject <- list.files( strProjectDirExp )
+    # nQtyFiles       <- length( vFilesInProject )
+    # nQtyFilesExp    <- 4
+ 
+    # expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
 
     # Now check the Calc package
-    strTmpDir       <- paste( strProjectDirExp, "/", strCalcName, sep = "" )
-    nQtyFilesExp    <- 8
-    nQtyFiles       <- length( list.files( strTmpDir ) )
-    expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the R library, project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in Calc project directory", tolerance = 0.1)
+    # strTmpDir       <- paste( strProjectDirExp, "/", strCalcName, sep = "" )
+    # nQtyFilesExp    <- 8
+    # nQtyFiles       <- length( list.files( strTmpDir ) )
+    # expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the R library, project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in Calc project directory", tolerance = 0.1)
+ 
 
-
-    # Now check the Shiny package
-    strTmpDir       <- paste( strProjectDirExp, "/", strShinyName, sep = "" )
-    nQtyFilesExp    <- 9
-    nQtyFiles       <- length( list.files( strTmpDir ) )
-    expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the Shiny project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in ShinyApp directory", tolerance = 0.1)
-
+    #Now check the Shiny package
+    # strTmpDir       <- paste( strProjectDirExp, "/", strShinyName, sep = "" )
+    # nQtyFilesExp    <- 9
+    # nQtyFiles       <- length( list.files( strTmpDir ) )
+    # expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the Shiny project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in ShinyApp directory", tolerance = 0.1)
+ 
     # For this  one it is important to make sure that the ShinyAppAsPakg/inst directory has the correct files
-    strTmpDir       <- paste( strProjectDirExp, "/", strShinyName, "/inst/TestShiny", sep = "" )
-    nQtyFilesExp    <- 13
-    nQtyFiles       <- length( list.files( strTmpDir ) )
-    expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the Shiny/inst project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in ShinyApp/inst directory", tolerance = 0.1)
-
-    CleanUpAfterTest( strProjDir )
+    # strTmpDir       <- paste( strProjectDirExp, "/", strShinyName, "/inst/TestShiny", sep = "" )
+    # nQtyFilesExp    <- 13
+    # nQtyFiles       <- length( list.files( strTmpDir ) )
+    # expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the Shiny/inst project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in ShinyApp/inst directory", tolerance = 0.1)
+ 
+    # CleanUpAfterTest( strProjDir )
 
 
 })
--- tests/testthat/test-CreateBaSSShinyAppAsPkg.R
+++ tests/testthat/test-CreateBaSSShinyAppAsPkg.R
@@ -3,49 +3,49 @@
 #################################################################################################### .
 
 
-context( "CratePREPShinyAppAsPkg")
+# context( "CratePREPShinyAppAsPkg")
 
-source( "MiscTestFunctions.R")
+# source( "MiscTestFunctions.R")
 
 test_that("Test- CreatePREPShinyAppAsPkg", {
 
 
-    bSuccess <- FALSE
-    strErr   <- "None of the projects were created."
+    # bSuccess <- FALSE
+    # strErr   <- "None of the projects were created."
 
-    strWD               <- getwd()
-    strProjDir          <- paste( strWD, "TestProject", sep = "/")
+    # strWD               <- getwd()
+    # strProjDir          <- paste( strWD, "TestProject", sep = "/")
 
-    CleanUpAfterTest( strProjDir ) # Clean up just in case a previous test failed and could not clean up
-    strProjName         <- "TestProj"
-    strShinyName        <- "TestShiny"
-    strShinyDispName    <- "Test App"
+    # CleanUpAfterTest( strProjDir ) # Clean up just in case a previous test failed and could not clean up
+    # strProjName         <- "TestProj"
+    # strShinyName        <- "TestShiny"
+    # strShinyDispName    <- "Test App"
 
-    strRet <-  CreatePREPShinyAppAsPkg( strProjDir, strShinyName  )
-    print( strRet )
+    # strRet <-  CreatePREPShinyAppAsPkg( strProjDir, strShinyName  )
+    # print( strRet )
 
-    strTmpDir       <- paste( strWD, "TestProject", strShinyName, sep = "/")
-    nQtyFilesExp    <- 9
-    nQtyFiles       <- length( list.files( strTmpDir ) )
-    expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the Shiny project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
-
-
-    strProjectFile <- paste( strTmpDir, "TestShiny.Rproj", sep = "/" )
-    expect_true( file.exists( strProjectFile ), info = "Failed to rename the project file name." )
-
-    strProjectFile <- paste( strTmpDir, "inst/TestShiny", sep = "/" )
-    expect_true( file.exists( strProjectFile ), info = "Failed to rename or create the shiny project file in the inst directory." )
+    # strTmpDir       <- paste( strWD, "TestProject", strShinyName, sep = "/")
+    # nQtyFilesExp    <- 9
+    # nQtyFiles       <- length( list.files( strTmpDir ) )
+    # expect_equal( nQtyFiles, nQtyFilesExp, info = paste( "Number of files in the Shiny project directory, expected", nQtyFilesExp, ", returned:", nQtyFiles), label ="Qty of files in project directory", tolerance = 0.1)
 
 
-    strProjectFile <- paste( strTmpDir, "R/RunApp.R", sep = "/" )
-    expect_true( file.exists( strProjectFile ), info = "Failed to copy or create the R/RunApp.R file." )
+    # strProjectFile <- paste( strTmpDir, "TestShiny.Rproj", sep = "/" )
+    # expect_true( file.exists( strProjectFile ), info = "Failed to rename the project file name." )
 
-    strFileLines       <- readLines( strProjectFile  )
+    # strProjectFile <- paste( strTmpDir, "inst/TestShiny", sep = "/" )
+    # expect_true( file.exists( strProjectFile ), info = "Failed to rename or create the shiny project file in the inst directory." )
 
-    vMatch   <- regexpr( strShinyName, strFileLines)
-    bUpdated <- any( vMatch != -1 )
-    expect_true( bUpdated, info = "Failed to update the R/RunApp.R file to contain the name of the Shiny app" )
 
-    CleanUpAfterTest( strProjDir )
+    # strProjectFile <- paste( strTmpDir, "R/RunApp.R", sep = "/" )
+    # expect_true( file.exists( strProjectFile ), info = "Failed to copy or create the R/RunApp.R file." )
+
+    # strFileLines       <- readLines( strProjectFile  )
+
+    # vMatch   <- regexpr( strShinyName, strFileLines)
+    # bUpdated <- any( vMatch != -1 )
+    # expect_true( bUpdated, info = "Failed to update the R/RunApp.R file to contain the name of the Shiny app" )
+
+    # CleanUpAfterTest( strProjDir )
 
 })
